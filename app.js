a9e6e091ed224daedcd9f6e479354b9ecd8f8786
Gunjan <34475432+DaNgLiN@users.noreply.github.com>
2019-08-26 14:18:48 +0530

    added npm systeminformation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+function timeplay(){
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime ="Date:"+ date+" | "+time;
 
 document.getElementById("clockview").innerText = dateTime;
+}
+setInterval(timeplay,1000)
+
