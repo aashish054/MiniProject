d49947f97cadd58b4b175b3f39a2f07be2c11df0
Gunjan <34475432+DaNgLiN@users.noreply.github.com>
2019-08-28 16:21:39 +0530

    Revert "added npm systeminformation"
    This reverts commit a9e6e091ed224daedcd9f6e479354b9ecd8f8786.

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-function timeplay(){
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime ="Date:"+ date+" | "+time;
 
 document.getElementById("clockview").innerText = dateTime;
-}
-setInterval(timeplay,1000)
-
