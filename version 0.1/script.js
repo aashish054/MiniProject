f84bcebf5fd95939689d847ec5388f68762bfbfe
aashish054 <aashishrana5@gmail.com>
2019-08-30 23:43:50 +0530

    sad

--- version 0.1/script.js
+++ version 0.1/script.js
@@ -1,9 +1,7 @@
 var rollOut = new Array()
 rollOut[0] = 0
 roll = 1 
-// for page translation
 function display_page(page_id){
-    display_menu(page_id)
     if(rollOut[rollOut.length-1]==page_id)
         return null
     rollOut[roll++] = page_id 
@@ -20,7 +18,10 @@ function display_page(page_id){
     page_no[rollOut[rollOut.length-1]].classList.add('rollIn')
     page_no[rollOut[rollOut.length-1]].style.display = 'block'
     page_no[rollOut[rollOut.length-2]].style.display = 'block'
-}
+     
+        
+      
+   
 
 //for menu color and border
     function display_menu(menu_id){
