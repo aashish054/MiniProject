23bafe2476136f63710a10a7c416560462eed609
aashish054 <aashishrana5@gmail.com>
2019-08-30 21:17:44 +0530

    roll in

--- version 0.1/script.js
+++ version 0.1/script.js
@@ -1,6 +1,17 @@
 function display_page(page_id){
+    
     var page_no = document.querySelectorAll('.page')
-        for(var i=0;i<page_no.length;i++)
+        for(var i=0;i<page_no.length;i++){
             page_no[i].style.display = 'none'
-    page_no[page_id].style.display = 'block'   
+      
+        }
+    var rollOut = null
+    page_no[page_id].style.display = 'block'
+    page_no[rollOut].classList.remove('rollIn') 
+    page_no[page_id].classList.add('rollOut')
+    page_no[page_id].classList.all('rollIn')    
+    setTimeout(function(){
+        page_no[rollOut].style.display = 'none'
+    },1000)   
+
 }
