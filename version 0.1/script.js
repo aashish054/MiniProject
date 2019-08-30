f57e740ec1cdf793028368781863c0f623a41c52
aashish054 <aashishrana5@gmail.com>
2019-08-30 22:29:04 +0530

    double roll

--- version 0.1/script.js
+++ version 0.1/script.js
@@ -1,17 +1,28 @@
+var rollOut = new Array()
+rollOut[0] = 0
+roll = 1 
 function display_page(page_id){
-    
+    if(rollOut[rollOut.length-1]==page_id)
+        return null
+    rollOut[roll++] = page_id 
+   
+   console.log(rollOut)
     var page_no = document.querySelectorAll('.page')
         for(var i=0;i<page_no.length;i++){
             page_no[i].style.display = 'none'
-      
         }
-    var rollOut = null
-    page_no[page_id].style.display = 'block'
-    page_no[rollOut].classList.remove('rollIn') 
-    page_no[page_id].classList.add('rollOut')
-    page_no[page_id].classList.all('rollIn')    
-    setTimeout(function(){
-        page_no[rollOut].style.display = 'none'
-    },1000)   
+        setTimeout(function(){
+            page_no[rollOut[rollOut.length-2]].style.display = 'none'
+            page_no[rollOut[rollOut.length-2]].classList.remove('rollOut')
+        },500)   
+    page_no[rollOut[rollOut.length-2]].classList.remove('rollIn')
+    page_no[rollOut[rollOut.length-2]].classList.add('rollOut')
+    page_no[rollOut[rollOut.length-1]].classList.add('rollIn')
+    page_no[rollOut[rollOut.length-1]].style.display = 'block'
+    page_no[rollOut[rollOut.length-2]].style.display = 'block'
+     
+        
+      
+   
 
 }
